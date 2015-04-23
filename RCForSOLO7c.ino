27aadc116e74e010d6e78379ecd4ef58188fd383
g1ts <coder@g1ts.net>
2015-04-23 13:17:34 +0500

    v1

---
+++ RCForSOLO7c.ino
@@ -0,0 +1,59 @@
+/*
+ * An IR LED must be connected to Arduino PWM pin 3.
+ * (for Mega pin 9, for other see in IRremoteInt.h)
+ */
+
+#include <IRremote.h>
+
+IRsend irsend;
+
+void setup(){
+  Serial.begin(9600);
+}
+
+
+int c;
+
+void loop() {
+  c = Serial.read();
+  if (c != -1) {
+    switch (c) {
+        case 0x31: // 1 - muteBtn
+            irsend.sendNEC(0x807f40bf, 32);
+            break;
+        case 0x32: // 2 - vupBtn
+            irsend.sendNEC(0x807F8877, 32);
+            break;
+        case 0x33: // 3 - vdownBtn
+            irsend.sendNEC(0x807F08F7, 32);
+            break;
+        case 0x34: // 4 - tUpBtn
+            irsend.sendNEC(0x807F906F, 32);
+            break;
+        case 0x35: // 5 - tDownBtn
+            irsend.sendNEC(0x807FB04F, 32);
+            break;
+        case 0x36: // 6 - bUpBtn
+            irsend.sendNEC(0x807F50AF, 32);
+            break;
+        case 0x37: // 7 - bDownBtn
+            irsend.sendNEC(0x807F708F, 32);
+            break;
+        case 0x38: // 8 - resetBtn
+            irsend.sendNEC(0x807F10EF, 32);
+            break;
+        case 0x39: // 9 - inputBtn
+            irsend.sendNEC(0x807F20DF, 32);
+            break;
+        default:
+            Serial.println("1 - mute");
+            Serial.println("2 - vup \t3 - vdown");
+            Serial.println("4 - tup \t5 - tdown");
+            Serial.println("6 - bup \t7 - bdown");
+            Serial.println("8 - reset\t9 - input");
+            break;
+    }
+    Serial.println("ok");
+    delay(40);
+  }
+}
